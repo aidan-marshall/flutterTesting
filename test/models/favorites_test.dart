462a00c45d0c71e41141d8ffb08ca4fcc7b7af38
Aidan Marshall <aimarsh022@student.wethinkcode.co.za>
2023-11-01 14:45:56 +0200

    added stuff

--- test/models/favorites_test.dart
+++ test/models/favorites_test.dart
@@ -10,5 +10,13 @@ void main() {
       favorites.add(number);
       expect(favorites.items.contains(number), true);
     });
+
+    test('An item should be removed', () {
+      var number = 45;
+      favorites.add(number);
+      expect(favorites.items.contains(number), true);
+      favorites.remove(number);
+      expect(favorites.items.contains(number), false);
+    });
   });
 }
